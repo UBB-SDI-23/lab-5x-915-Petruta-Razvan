0f5e9d61448d2e1d1d573927cf291e5913c9ab2d
915-Petruta-Razvan <razvan.petruta@stud.ubbcluj.ro>
2023-04-14 20:48:37 +0300

    update pom

--- LibraryBackend/src/main/java/com/example/restapi/RestApiApplication.java
+++ LibraryBackend/src/main/java/com/example/restapi/RestApiApplication.java
@@ -2,10 +2,17 @@ package com.example.restapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RestApiApplication {
+public class RestApiApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(RestApiApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder applicationBuilder) {
+		return applicationBuilder.sources(RestApiApplication.class);
 	}
 }
