4863ad1065266bce68ad22a72da19cfc7f1318af
915-Petruta-Razvan <razvan.petruta@stud.ubbcluj.ro>
2023-04-15 11:30:11 +0300

    try one more time

--- LibraryBackend/src/main/java/com/example/restapi/RestApiApplication.java
+++ LibraryBackend/src/main/java/com/example/restapi/RestApiApplication.java
@@ -2,17 +2,10 @@ package com.example.restapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RestApiApplication extends SpringBootServletInitializer {
+public class RestApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RestApiApplication.class, args);
-	}
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(RestApiApplication.class);
 	}
 }
